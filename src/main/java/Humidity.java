034d3f93fa551c90663355728063b9250ae6e13f
MagdaZny <zieleznym6@gmail.com>
2017-02-18 18:17:53 +0100

    Assignment for week 3

--- src/main/java/Humidity.java
+++ src/main/java/Humidity.java
@@ -1,7 +1,61 @@
-package PACKAGE_NAME;
+import edu.duke.DirectoryResource;
+import edu.duke.FileResource;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
-/**
- * Created by magdazielezny on 18/02/2017.
- */
+import java.io.File;
+
 public class Humidity {
+
+    public static CSVRecord lowestInOneDay(CSVParser parser) {
+
+        CSVRecord lowestSoFar = null;
+        String time = "";
+        Double lowestHum = 0.0;
+
+        for (CSVRecord currentRow : parser) {
+            lowestSoFar = getLowestOfTwo(lowestSoFar, currentRow);
+        }
+        return lowestSoFar;
+    }
+
+
+    public static CSVRecord lowestInManyDays() {
+
+        DirectoryResource dr = new DirectoryResource();
+        CSVRecord lowestSoFar = null;
+
+        for (File f : dr.selectedFiles()) {
+            FileResource file = new FileResource(f);
+            CSVRecord currentRow = lowestInOneDay(file.getCSVParser());
+            lowestSoFar=getLowestOfTwo(lowestSoFar, currentRow);
+        }System.out.println(lowestSoFar.get("Humidity")+" : "+lowestSoFar.get("DateUTC"));
+
+        return lowestSoFar;
+    }
+
+    public static CSVRecord getLowestOfTwo(CSVRecord lowestSoFar, CSVRecord currentRow) {
+        if (lowestSoFar == null) {
+            lowestSoFar = currentRow;
+        } else {
+            Double currentTemp = Double.parseDouble(currentRow.get("Humidity"));
+            Double largestTemp = Double.parseDouble(lowestSoFar.get("Humidity"));
+            if (currentTemp < largestTemp) {
+                lowestSoFar = currentRow;
+            }
+        }
+        return lowestSoFar;
+    }
+
+    public static void main(String[] arg){
+//        FileResource file = new FileResource("weather-2014-07-22.csv");
+//        CSVParser parser = file.getCSVParser();
+//        CSVRecord largest = hottestInOneDay(file.getCSVParser());
+//        System.out.println("The hottest time in one day was "+ largest.get("TimeEST")+" with temperature "+largest.get("TemperatureF"));
+//        CSVRecord largestInManyDays = hottestInManyDays();
+//        System.out.println("The hottest time in many days was "+ largestInManyDays.get("TimeEST")+" with temperature "+largestInManyDays.get("TemperatureF"));
+
+        //    lowestHumidity(parser);
+        lowestInManyDays();
+    }
 }
--- src/main/java/Temperature.java
+++ src/main/java/Temperature.java
@@ -12,47 +12,117 @@ public class Temperature {
         CSVRecord largestSoFar = null;
 
         for (CSVRecord currentRow : parser) {
-            if (largestSoFar == null) {
+             largestSoFar = getHottestOfTwo(largestSoFar, currentRow);
+        }
+        return largestSoFar;
+    }
+
+        public static CSVRecord coldestInOneDay(CSVParser parser) {
+
+            CSVRecord coldestSoFar = null;
+
+            for (CSVRecord currentRow : parser) {
+                coldestSoFar = getColdestOfTwo(coldestSoFar, currentRow);
+            }
+            return coldestSoFar;
+        }
+
+    public static CSVRecord hottestInManyDays() {
+
+        DirectoryResource dr = new DirectoryResource();
+        CSVRecord largestSoFar = null;
+
+        for (File f : dr.selectedFiles()) {
+            FileResource file = new FileResource(f);
+            CSVRecord currentRow = hottestInOneDay(file.getCSVParser());
+            largestSoFar=getHottestOfTwo(largestSoFar, currentRow);
+        }return largestSoFar;
+    }
+
+    public static String coldestDayInManyDays() {
+
+        DirectoryResource dr = new DirectoryResource();
+        CSVRecord coldestSoFar = null;
+        String date=null;
+        String temp=null;
+
+        for (File f : dr.selectedFiles()) {
+            FileResource file = new FileResource(f);
+            CSVRecord currentRow = coldestInOneDay(file.getCSVParser());
+            coldestSoFar=getColdestOfTwo(coldestSoFar, currentRow);
+            date = coldestSoFar.get("DateUTC");
+            temp = coldestSoFar.get("TemperatureF");
+        }
+
+        return temp;
+    }
+
+    public static CSVRecord getHottestOfTwo(CSVRecord largestSoFar, CSVRecord currentRow) {
+        if (largestSoFar == null) {
+            largestSoFar = currentRow;
+        } else {
+            Double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+            Double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+            if (currentTemp > largestTemp) {
                 largestSoFar = currentRow;
-            } else {
-                Double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
-                Double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
-                if (currentTemp > largestTemp) {
-                    largestSoFar = currentRow;
-                }
             }
         }
         return largestSoFar;
     }
-    public static CSVRecord hottestInManyDays() {
 
-        DirectoryResource dr = new DirectoryResource();
-
-        CSVRecord largestSoFar = null;
-        for (File f : dr.selectedFiles()) {
-
-            FileResource file = new FileResource(f);
-            if (largestSoFar == null) {
-                largestSoFar = hottestInOneDay(file.getCSVParser());
-            } else {
-                CSVRecord current = hottestInOneDay(file.getCSVParser());
-                Double currentTemp = Double.parseDouble(current.get("TemperatureF"));
-                Double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
-                if (currentTemp > largestTemp) {
-                    largestSoFar = current;
-                }
+    public static CSVRecord getColdestOfTwo(CSVRecord coldestSoFar, CSVRecord currentRow) {
+        if (coldestSoFar == null) {
+            coldestSoFar = currentRow;
+        } else {
+            Double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+            Double coldestTemp = Double.parseDouble(coldestSoFar.get("TemperatureF"));
+            if (currentTemp < coldestTemp) {
+                coldestSoFar = currentRow;
             }
-        }return largestSoFar;
+        }
+        return coldestSoFar;
+    }
+    public static double getAverageTemp(CSVParser parser){
+
+        double sum = 0;
+        int counter=0;
+
+        for (CSVRecord record: parser){
+            double currentTemp = Double.parseDouble(record.get("TemperatureF"));
+            sum+=currentTemp;
+            counter++;
+        }
+        return sum/counter;
     }
 
 
+    public static float getAverageTempWhenHumidity(CSVParser parser){
+
+        int counter=0;
+        float sum=0;
+
+        for (CSVRecord record: parser){
+            float humidity = Float.parseFloat(record.get("Humidity"));
+            float curentTemp=Float.parseFloat(record.get("TemperatureF"));
+            if (humidity >= 80){
+                sum +=curentTemp;
+                counter++;}
+        }
+        System.out.println("Counter: "+ counter);
+        float average=sum/counter;
+        return average;
+    }
+
     public static void main(String[] arg){
-        FileResource file = new FileResource("data/2015/weather-2015-01-01.csv");
-        CSVRecord largest = hottestInOneDay(file.getCSVParser());
-        System.out.println("The hottest time was "+ largest.get("TimeEST")+" with temperature "+largest.get("TemperatureF"));
-        CSVRecord largestInManyDays = hottestInManyDays();
-        System.out.println("The hottest time was "+ largestInManyDays.get("TimeEST")+" with temperature "+largestInManyDays.get("TemperatureF"));
+        FileResource file = new FileResource("weather-2013-09-02.csv");
+        CSVParser parser = file.getCSVParser();
+//        CSVRecord largest = hottestInOneDay(file.getCSVParser());
+//        System.out.println("The hottest time in one day was "+ largest.get("TimeEST")+" with temperature "+largest.get("TemperatureF"));
+//        CSVRecord largestInManyDays = hottestInManyDays();
+//        System.out.println("The hottest time in many days was "+ largestInManyDays.get("TimeEST")+" with temperature "+largestInManyDays.get("TemperatureF"));
 
-
+        //System.out.println("The average temp was " + getAverageTemp(parser));
+        System.out.println("The average temp when humidity was 80 or more was " + getAverageTempWhenHumidity(parser));
+        System.out.print("The coldest day was "+ coldestDayInManyDays());
     }
 }
