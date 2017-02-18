7727ec3cf0d703512d7dc8f7a566da3c41f34378
MagdaZny <zieleznym6@gmail.com>
2017-02-18 18:16:19 +0100

    add Humidity class

---
+++ src/main/java/Humidity.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by magdazielezny on 18/02/2017.
+ */
+public class Humidity {
+}
--- src/main/java/WhichCountriesExport.java
+++ src/main/java/WhichCountriesExport.java
@@ -2,14 +2,28 @@ import edu.duke.FileResource;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
+import java.io.IOException;
+
 public class WhichCountriesExport {
 
-    public static void listExporters(CSVParser parser, String exportOfInterest){
+    public static void listExporters(CSVParser parser, String exportOfInterest1, String exportOfInterest2){
 
         int count = 0;
         for (CSVRecord record: parser){
             String exports = record.get("Exports");
-            if (exports.contains(exportOfInterest)){
+            if (exports.contains(exportOfInterest1)&&exports.contains(exportOfInterest2)){
+                System.out.println(record.get("Country"));
+                count++;
+            }
+        }
+        System.out.println(count + " exporters.");
+    }
+    public static void listExporters(CSVParser parser, String exportOfInterest1){
+
+        int count = 0;
+        for (CSVRecord record: parser){
+            String exports = record.get("Exports");
+            if (exports.contains(exportOfInterest1)){
                 System.out.println(record.get("Country"));
                 count++;
             }
@@ -17,24 +31,30 @@ public class WhichCountriesExport {
         System.out.println(count + " exporters.");
     }
 
-    public static void countryDetails(CSVParser parser, String country){
+    public static void countryDetails(CSVParser parser){
 
             for(CSVRecord record: parser){
-                String c = record.get("Country");
-                //if (c.equals(country)) {
-                    String exports = record.get("Exports");
+                    String country = record.get("Country");
                     String value = record.get("Value (dollars)");
-                    System.out.println(country + ": " + exports + " : " + value);
+                    String valueNumber = value.replaceAll("\\D+","");
+                long valueLong = Long.parseLong(valueNumber);
+                if(valueLong> 999999999999L){
+                    System.out.println(country + " : " + valueNumber);
                 }
+                }
+
         }
 
-    public static void main(String[] arg){
 
-        FileResource file = new FileResource("exports_small.csv");
+    public static void main(String[] arg) throws IOException {
+
+        FileResource file = new FileResource("exportdata.csv");
         CSVParser parser = file.getCSVParser();
 
-        listExporters(parser, "coffe");
+       listExporters(parser, "cotton", "flowers");
+       listExporters(parser, "cocoa");
+        System.out.println("");
 
-         countryDetails(parser, "Germany");
+        countryDetails(parser);
     }
 }
